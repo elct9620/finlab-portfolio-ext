f7fa98d7b620c00480be08442ef9f962e7a1279a
Aotokitsuruya <contact@aotoki.me>
2024-08-11 19:25:10 +0800

    Add Finlab PortfolioSyncManager as dependency

--- features/environment.py
+++ features/environment.py
@@ -1,12 +1,20 @@
 from behave import fixture, use_fixture
 from finlab_portfolio_ext import HybridManager
+from finlab.portfolio import PortfolioSyncManager
+
+
+@fixture
+def portfolio_manager(context):
+    context.portfolio_manager = PortfolioSyncManager()
+    yield context.portfolio_manager
 
 
 @fixture
 def manager(context):
-    context.manager = HybridManager()
+    context.manager = HybridManager(portfolio_manager=context.portfolio_manager)
     yield context.manager
 
 
 def before_scenario(context, scenario):
+    use_fixture(portfolio_manager, context)
     use_fixture(manager, context)
--- finlab_portfolio_ext/hybrid_manager.py
+++ finlab_portfolio_ext/hybrid_manager.py
@@ -1,3 +1,9 @@
+from finlab.portfolio import PortfolioSyncManager
+
+
 class HybridManager:
+    def __init__(self, portfolio_manager: PortfolioSyncManager):
+        self._pm = portfolio_manager
+
     def current_position(self):
-        return {}
+        return self._pm.get_position()
